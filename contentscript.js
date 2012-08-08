ddc895c3b8db07404a9c333e224b91415766caeb
Fan Yu <yufan@dev-yufan.visual-2000.cn>
2012-08-08 09:45:52 +0800

    contentscript

--- contentscript.js
+++ contentscript.js
@@ -1,2 +1,9 @@
 console.log("content script loaded");
-$("img").css("border", "5px solid orange");
+//$("img").css("border", "5px solid orange");
+function addborder(event) {
+    $(this).css("border", "5px solid orange");
+}
+function removeborder(event) {
+    $(this).css("border", "");
+}
+$("img").hover(addborder, removeborder);
